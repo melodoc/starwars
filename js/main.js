a919c684a31b0d626f9ac293ad29e3f19ace0039
melodoc <l_tih@ro.ru>
2020-05-21 12:32:02 +0300

    [bug] Add carousel

--- js/main.js
+++ js/main.js
@@ -1,29 +1,20 @@
 'use strict';
 
-const cardsepisodes = document.querySelector('.slider__card');
-
 const getData = async function (url) {
-
     const response = await fetch(url);
-
     if (!response.ok) {
         throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response, status}`)
     }
-
     return await response.json();
-
 };
 
 getData('./db/episodes.json');
 
-
 function createCardEpisode(episode) {
-
+    const cardsEpisodes = document.querySelector('.slider__card');
     const { year, rating, name, image } = episode;
     const card = document.createElement('div');
-
     card.classList.add('slider__card-item');
-
     card.info = [year, rating, name, image]
 
     card.insertAdjacentHTML('beforeend', `
@@ -32,9 +23,8 @@ function createCardEpisode(episode) {
              <div class="slider__card-name content">${name}</div>
              <div class="slider__card-rating content">${rating}</div>
  `);
-    cardsepisodes.insertAdjacentElement('beforeend', card);
+    cardsEpisodes.insertAdjacentElement('beforeend', card);
 }
-
 
 function init() {
     getData('./db/episodes.json').then(function (data) {
