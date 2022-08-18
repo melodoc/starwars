cac8f14a6196569a8d1200918f9aed415ac8dd92
Lyubov.Ryazanova <Lyubov.Ryazanova@waveaccess.ru>
2022-08-19 03:18:34 +0400

    [components] Add pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React, { useState } from 'react';
 import { Episodes } from './pages/episodes';
 import { Characters } from './pages/characters';
+import { Planets } from './pages/planets';
+import { Starship } from './pages/starship';
 import { Header } from './components/header';
 import { CurrentTabContext } from './contexts';
 import { TabsEnum } from './constants';
 
-const getBackgroundImage = (currentTab) => {
-  switch (currentTab) {
-    case TabsEnum.EPISODES:
-      return 'main-bg.jpg';
-    case TabsEnum.CHARACTERS:
-      return 'panel.jpg';
-    default:
-      return 'galaxy-bg.png';
-  }
-};
-
 export const App = () => {
   const [currentTab, setCurrentTab] = useState(TabsEnum.EPISODES);
   const value = { currentTab, setCurrentTab };
-
-  const backgroundImageUrl = getBackgroundImage(currentTab);
 
   return (
     <CurrentTabContext.Provider value={value}>
@@ -28,8 +17,8 @@ export const App = () => {
         <Header />
         {currentTab === TabsEnum.EPISODES && <Episodes />}
         {currentTab === TabsEnum.CHARACTERS && <Characters />}
-        {currentTab === TabsEnum.STARSHIP && <Episodes />}
-        {currentTab === TabsEnum.PLANETS && <Episodes />}
+        {currentTab === TabsEnum.STARSHIP && <Starship />}
+        {currentTab === TabsEnum.PLANETS && <Planets />}
       </div>
     </CurrentTabContext.Provider>
   );
