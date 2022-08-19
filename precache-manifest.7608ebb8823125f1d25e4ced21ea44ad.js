aed97bf5bdd9b267461464a3569e6ffaf94320e7
melodoc <melodoc@users.noreply.github.com>
2022-08-19 08:44:56 +0000

    Deploying to gh-pages from @ melodoc/starwars@7b02684a433e2bd99a926750fa235438e3ee94e6 🚀

---
+++ precache-manifest.7608ebb8823125f1d25e4ced21ea44ad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687ba14a42da61316c429760ae677021",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "b9d516cc7b6cac1876f9",
+    "url": "/starwars/static/css/main.2fcaeef2.chunk.css"
+  },
+  {
+    "revision": "d1f22d2e64d490e770ea",
+    "url": "/starwars/static/js/2.ef1351e0.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/starwars/static/js/2.ef1351e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74739afb724a2e39e793",
+    "url": "/starwars/static/js/3.76081591.chunk.js"
+  },
+  {
+    "revision": "b9d516cc7b6cac1876f9",
+    "url": "/starwars/static/js/main.162d9caf.chunk.js"
+  },
+  {
+    "revision": "cc47ba86a05c145527a1",
+    "url": "/starwars/static/js/runtime-main.ba8c9898.js"
+  },
+  {
+    "revision": "1131cb60600871f61c9022c17d10567a",
+    "url": "/starwars/static/media/left-arrow.1131cb60.svg"
+  },
+  {
+    "revision": "bbdcc29cc93e9889a68abe9d778e3120",
+    "url": "/starwars/static/media/main-bg.bbdcc29c.jpg"
+  },
+  {
+    "revision": "66700225633a048ce07916eb2ada602b",
+    "url": "/starwars/static/media/rating.66700225.svg"
+  },
+  {
+    "revision": "e86c09e468277b92752c0b4d31c7c4c7",
+    "url": "/starwars/static/media/right-arrow.e86c09e4.svg"
+  }
+]);
