5d4e33bd627b2742005203068ff844e689565f6d
melodoc <liubov_rz@yahoo.com>
2023-01-22 12:21:58 +0400

    [components] change body styles and move it to App component #28

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ import { Header } from './components/header';
 import { CurrentTabContext } from './contexts';
 import { TabsEnum } from './constants';
 
+import './App.css';
+
 export const App = () => {
   const [currentTab, setCurrentTab] = useState(TabsEnum.EPISODES);
   const value = { currentTab, setCurrentTab };
 
   return (
     <CurrentTabContext.Provider value={value}>
-      <div className="container">
-        <Header />
-        {currentTab === TabsEnum.EPISODES && <Episodes />}
-        {currentTab === TabsEnum.CHARACTERS && <Characters />}
-        {currentTab === TabsEnum.STARSHIP && <Starship />}
-        {currentTab === TabsEnum.PLANETS && <Planets />}
+      <div className="body">
+        <div className="container">
+          <Header />
+          {currentTab === TabsEnum.EPISODES && <Episodes />}
+          {currentTab === TabsEnum.CHARACTERS && <Characters />}
+          {currentTab === TabsEnum.STARSHIP && <Starship />}
+          {currentTab === TabsEnum.PLANETS && <Planets />}
+        </div>
       </div>
     </CurrentTabContext.Provider>
   );
